dd700f1d875bb0d1867f99f4581cf90d3d34338e
anacardona0220 <anamariacb0220@gmail.com>
2023-03-27 16:52:46 -0500

    _printf implementation of the formats

---
+++ _printf.c
@@ -0,0 +1,38 @@
+int _printf(const char *format, ...)
+{
+    va_list parametros;
+    char *p, *sval, *car;
+    int ival;
+    double dval;
+
+    va_start(parametros, format);
+    for (p = format; *p; p++)
+    {
+        if (*p != '%')
+        {
+            putchar(*p);
+            continue;
+        }
+        switch (*++p)
+        {
+        case 'd':
+            ival = va_arg(parametros, int);
+            printf("%d", ival);
+            break;
+        case 'f':
+            dval = va_arg(parametros, double);
+            printf("%f", dval);
+            break;
+        case 's':
+            for (sval = va_arg(parametros, char *); *sval; sval++)
+                putchar(*sval);
+            break;
+
+        default:
+            putchar(*p);
+            break;
+        }
+    }
+    va_end(parametros);
+    return;
+}
