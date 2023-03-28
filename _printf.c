65de6acf861b9a647484024b0cc71aeaa1489ed3
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-03-28 15:55:52 -0500

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,43 +1,35 @@
-#include <limits.h>
-#include <stdio.h>
-#include "main.h"
-/**
-* _printf - a function that produces output according to a format
-* _puntchar - function to print
-* _process_format - process the format
-*@format: char
-*description - function that produces output according to a format
-*Return: 0;
-*/
-int _process_format(char format, va_list argumentos)
-{
-int num = 0;
-if (format != 'd' && format != 'i' && format != 's' && format != 'f' && format != 'c'  && format != '%')
-{
-num += printf("%c", format);
-}
-else
-{
-switch (format)
-{
-case 'd':
-num += printf("%d", va_arg(argumentos, int));
-break;
-case 's':
-num += printf("%s", va_arg(argumentos, char *));
-break;
-case 'f':
-num += printf("%f", va_arg(argumentos, double));
-break;
-case 'c':
-num += printf("%c", va_arg(argumentos, char *));
-break;
-case 'i':
-num += printf("%i", va_arg(argumentos, int));
-break;
-default:
-return -1;
-}
-return (num);
-}
+#include <limits.h>                                                                                                                    
+#include <stdio.h>                                                                                                                     
+#include "main.h"                                                                                                                      
+/**                                                                                                                                    
+* _printf - a function that produces output according to a format                                                                      
+* _puntchar - function to print                                                                                                        
+* _process_format - process the format                                                                                                 
+*@format: char                                                                                                                         
+*description - function that produces output according to a format                                                                     
+*Return: 0;                                                                                                                            
+*/                                                                                                                                     
+int _printf(const char *format, ...)                                                                                                   
+{                                                                                                                                      
+va_list argumentos;                                                                                                                    
+int num = 0;                                                                                                                           
+if (format == NULL)                                                                                                                    
+return (-1);                                                                                                                           
+va_start(argumentos, format);                                                                                                          
+while (*format != '\0')                                                                                                                
+{                                                                                                                                      
+if (*format == '%')                                                                                                                    
+{                                                                                                                                      
+format++;                                                                                                                              
+num += _process_format(*format, argumentos);                                                                                           
+}                                                                                                                                      
+else                                                                                                                                   
+{                                                                                                                                      
+_putchar(*format);                                                                                                                     
+num++;                                                                                                                                 
+}                                                                                                                                      
+format++;                                                                                                                              
+}                                                                                                                                      
+va_end(argumentos);                                                                                                                    
+return (num);                                                                                                                          
 }
