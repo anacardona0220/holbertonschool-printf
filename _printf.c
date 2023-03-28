67cb35dc1b8f7d7fae12978268dbfdc3176b04ba
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-28 17:39:42 -0500

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,37 +1,36 @@
-#include <limits.h>
-#include <stdio.h>
-#include "main."
-#include <stdarg.h>
-/**
-* _printf - a function that produces output according to a format
-* _puntchar - function to print
-* _process_format - process the format
-*@format: char
-*description - function that produces output according to a format
-*Return: 0;
-*/
-int _printf(const char *format, ...)
-{
-	va_list argumentos;
-	int num = 0;
-	if (format == NULL)
-		return (-1);
-	va_start(argumentos, format);
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			num += _process_format(*format, argumentos);
-		}
-		else
-		{
-			putchar(*format);
-			num++;
-		}
-		format++;
-	}
-	va_end(argumentos);
-
-	return (num);
-}
+#include <limits.h>                                                                             
+#include <stdio.h>                                                                              
+#include "main."                                                                                
+#include <stdarg.h>                                                                             
+/**                                                                                             
+* _printf - a function that produces output according to a format                               
+* _puntchar - function to print                                                                 
+* _process_format - process the format                                                          
+*@format: char                                                                                  
+*description - function that produces output according to a format                              
+*Return: 0;                                                                                     
+*/                                                                                              
+int _printf(const char *format, ...)                                                            
+{                                                                                               
+va_list argumentos;                                                                             
+int num = 0;                                                                                    
+if (format == NULL)                                                                             
+return (-1);                                                                                    
+va_start(argumentos, format);                                                                   
+while (*format != '\0')                                                                         
+{                                                                                               
+if (*format == '%')                                                                             
+{                                                                                               
+format++;                                                                                       
+num += _process_format(*format, argumentos);                                                    
+}                                                                                               
+else                                                                                            
+{                                                                                               
+_putchar(*format);                                                                              
+num++;                                                                                          
+}                                                                                               
+format++;                                                                                       
+}                                                                                               
+va_end(argumentos);                                                                             
+return (num);                                                                                   
+} 
