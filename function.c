58a6fccbff890ab232d50165bcae169d0419107c
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-28 13:48:18 -0500

    Update function.c

--- function.c
+++ function.c
@@ -30,7 +30,7 @@ case 'i':
 num += printf("%i", va_arg(argumentos, int));
 break;
 default:
-fprintf(stderr, "Formato inválido: %c\n", format);
+fprintf("Formato inválido: %%\n", format);
 return -1;
 }
 return num;
