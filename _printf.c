76096a65bcc701963fe95eb95bf7bedc55ed79a9
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-03-28 10:31:19 -0500

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,36 +1,32 @@
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
-
+/*_puntchar - function to print
+ *_process_format - process the format
+ *@format: char
+ *@num: int
+*/
 int _printf(const char *format, ...)
 {
-    va_list argumentos;
-    int num = 0;
-
-    if (format == NULL)
-        return (-1);
-
-    va_start(argumentos, format);
-
-    while (*format != '\0')
-
-    {
-        if (*format == '%')
-
-        {
-            format++;
-
-            num += _process_format(*format, argumentos);
-        }
-        else
-        {
-            _putchar(*format);
-            num++;
-        }
-        format++;
-    }
-
-    va_end(argumentos);
-
-    return (num);
+va_list argumentos;
+int num = 0;
+if (format == NULL)
+return (-1);
+va_start(argumentos, format);
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+num += _process_format(*format, argumentos);
+}
+else
+{
+_putchar(*format);
+ num++;
+ }
+ format++;
+ }
+va_end(argumentos);
+return (num);
 }
