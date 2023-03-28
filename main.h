bda6ad8bf0f606b6a3e1e3422678cac5292bbb00
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-28 10:52:11 -0500

    Update main.h

--- main.h
+++ main.h
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdarg.h>
-/*_puntchar - function to print
- *_process_format - process the format
- *@format: char
+/*
+* _puntchar - function to print
+* _printf - funtion printf
+*_process_format - process the format
+*@format: char
 */
-
+int _printf(const char *format, ...);
 int _putchar(char c)
 {
 return putchar(c);
