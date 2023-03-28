f3d6c202641e18ae3a95bfcfeb4ae0dc8b68937b
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-28 17:38:48 -0500

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -12,25 +12,26 @@
 */
 int _printf(const char *format, ...)
 {
-va_list argumentos;
-int num = 0;
-if (format == NULL)
-return (-1);
-va_start(argumentos, format);
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-num += _process_format(*format, argumentos);
-}
-else
-{
-_putchar(*format);
-num++;
-}
-format++;
-}
-va_end(argumentos);
-return (num);
+	va_list argumentos;
+	int num = 0;
+	if (format == NULL)
+		return (-1);
+	va_start(argumentos, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			num += _process_format(*format, argumentos);
+		}
+		else
+		{
+			putchar(*format);
+			num++;
+		}
+		format++;
+	}
+	va_end(argumentos);
+
+	return (num);
 }
