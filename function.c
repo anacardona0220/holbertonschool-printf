34e9933f3ffea661b0eaf45025afaed4d67c0229
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-03-29 06:41:55 -0700

    corection

--- function.c
+++ function.c
@@ -25,22 +25,14 @@ case 'f':
 num += printf("%f", va_arg(argumentos, double));
 break;
 case 'c':
-<<<<<<< HEAD
 num += printf("%c", va_arg(argumentos, int));
-=======
-num += printf("%c", va_arg(argumentos, char *));
->>>>>>> 15608fbea5d66aa8df1109767f2b1d7ca879cf41
 break;
 case 'i':
 num += printf("%i", va_arg(argumentos, int));
 break;
 default:
-<<<<<<< HEAD
 _putchar(37);
 _putchar(format);
-=======
-fprintf(stderr "formato invalido : %c/n", format);
->>>>>>> 15608fbea5d66aa8df1109767f2b1d7ca879cf41
 return -1;
 }
 return (num);
