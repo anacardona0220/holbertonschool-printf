bf6034a60f6f457988e809c219efe674b9c15ab0
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-29 17:15:37 -0500

    Update main.h

--- main.h
+++ main.h
@@ -11,7 +11,7 @@ typedef struct data
   char *tipo;
   int (*f)(va_list);
 } data_t;
-int _putcharf(char c);
+int _putcharb(char c);
 int parser(const char *format, data_t fn[], va_list arg_list);
 int _printf(const char *format, ...);
 int p_c(va_list);
