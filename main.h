f6fcaf1ee4606d8d241506ab30f835ca2904f45e
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-03-29 13:00:22 -0500

    Update main.h

--- main.h
+++ main.h
@@ -1,15 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
-/*
-* _printf - funtion printf
-*_process_format - process the format
-*@format: char
-*/
-int _putchar(char c);
-int _process_format(char format, va_list argumentos);
+
+/**
+ * struct format - Struct format
+ *
+ * @type: The conversion specifier
+ * @func: The function pointer to the right function
+ */
+typedef struct format
+{
+    char *type;
+    int (*func)(va_list);
+} format_t;
 int _printf(const char *format, ...);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_percent(va_list arg);
+int print_int(va_list arg);
+int print_unsigned(va_list arg);
+int print_octal(va_list arg);
+int print_hex(va_list arg);
+int print_HEX(va_list arg);
+int print_binary(va_list arg);
 
 #endif
