914e1998c387c73963bb87508802ca8efb15d9ac
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-29 17:55:46 -0500

    Update parser.c

--- parser.c
+++ parser.c
@@ -52,3 +52,15 @@ printed_chars++;
 }
 return (printed_chars);
 }
+
+
+/**
+ * print_percent - Prints a percent symbol
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
+ */
+int print_percent(__attribute__((unused))va_list list)
+{
+	_putcharf('%');
+	return (1);
+}
