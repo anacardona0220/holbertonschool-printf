89efb49b134a89f2ae3c0a1940ad38c7cd07db2e
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-03-29 16:33:56 -0500

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,3 +1,15 @@
+#include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * p_d - its used to print integer and decimal
+ * @args: parameter
+ *
+ *
+ * Return: 0
+*/
 int p_d_i(va_list list)
 {
 int nLength;
@@ -30,5 +42,27 @@ length += _putcharf('0' + num / div);
 num %= div;
 div /= 10;
 }
-  return (length);
+return (length);
 }
+int p_c(va_list list)
+{
+_putcharf(va_arg(list, int));
+return (1);
+}
+int p_s(va_list list)
+{
+int i;
+char *str;
+str = va_arg(list, char *);
+if (str == NULL)
+str = "(null)";
+for (i = 0; str[i] != '\0'; i++)
+_putcharf(str[i]);
+return (i);
+}
+int p_p(__attribute__((unused))va_list list)
+{
+_putcharf('%');
+return (1);
+}
+
