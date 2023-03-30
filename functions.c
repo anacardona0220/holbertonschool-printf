4a33cc48f5ace4616f0c2c9cd56c201e8e350314
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-03-30 06:57:47 -0700

    create fn printString

--- functions.c
+++ functions.c
@@ -67,21 +67,3 @@ int print_char(va_list list)
 _putcharf(va_arg(list, int));
 return (1);
 }
-
-/**
- * print_string - Prints a string
- * @list: list of arguments
- * Return: Will return the amount of characters printed.
- */
-int print_string(va_list list)
-{
-	int i;
-	char *str;
-
-	str = va_arg(list, char *);
-	if (str == NULL)
-		str = "(null)";
-	for (i = 0; str[i] != '\0'; i++)
-		_putcharf(str[i]);
-	return (i);
-}
