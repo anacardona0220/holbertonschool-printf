458a2b5b30d5323326dbb7688f564cd11b855ad4
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-30 10:49:18 -0500

    Update parser.c

--- parser.c
+++ parser.c
@@ -21,65 +21,34 @@ for (k = 0; format[k] != '\0'; k++)
 if (format[k] == '%')
 {
 for (j = 0; f_list[j].rm != NULL; j++)
-	    
-	    {
-	      
-	      if (format[k + 1] == f_list[j].rm[0])
-		
-		{
-		  
-		  newVal = f_list[j].f(arg_list);
-		  
-		  if (newVal == -1)
-		    
-		    return (-1);
-		  
-		  printData += newVal;
-		  
-		  break;
-		  
-		}
-	      
-	    }
-	  
-	  if (f_list[j].rm == NULL && format[k + 1] != ' ')
-	    
-	    {
-	      
-	      if (format[k + 1] != '\0')
-		
-		{
-		  
-		  _putcharf(format[k]);
-		  
-		  _putcharf(format[k + 1]);
-		  
-		  printData = printData + 2;
-		  
-		}
-	      
-	      else
-		
-		return (-1);
-	      
-	    }
-	  
-	  k = k + 1; /*Updating i to skip format symbols*/
-	  
-	}
-      
-      else
-	
-	{
-	  
-	  _putcharf(format[k]); /*call the write function*/
-	  
-	  printData++;
-	  
-	}
-      
-    }
-  
-  return (printData);
-  
+{
+if (format[k + 1] == f_list[j].rm[0])
+{
+newVal = f_list[j].f(arg_list);
+if (newVal == -1)
+return (-1);
+printData += newVal;
+break;
+}
+}
+if (f_list[j].rm == NULL && format[k + 1] != ' ')
+{
+if (format[k + 1] != '\0')
+{
+_putcharf(format[k]);
+_putcharf(format[k + 1]);
+printData = printData + 2;
+}
+else
+return (-1);
+}
+k = k + 1; /*Updating i to skip format symbols*/
+}
+else
+{
+_putcharf(format[k]); /*call the write function*/
+printData++;
+}
+}
+return (printData);
 }
